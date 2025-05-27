af128c5e312aad91566e481a9a12bb26bb4e3bb0
Jgtech1 <123068370+Jgtech1@users.noreply.github.com>
2025-05-27 03:26:04 -0700

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
+
+$(document).ready(function () {
+  $('#navToggleBtn').click(function () {
+    $('#fullscreenMenu')
+      .removeClass('animate__fadeOut d-none')
+      .addClass('show animate__fadeIn');
+  });
+
+  $('#closeMenu').click(function () {
+    $('#fullscreenMenu')
+      .removeClass('animate__fadeIn')
+      .addClass('animate__fadeOut');
+
+    setTimeout(function () {
+      $('#fullscreenMenu').removeClass('show').addClass('d-none');
+    }, 500); // match animation duration
+  });
+});
+
+
+
   document.getElementById('currentYear').textContent = new Date().getFullYear();
